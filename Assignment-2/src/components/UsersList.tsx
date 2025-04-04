5d81d7408b9aa2f802fa5453c232c93bf1a927cc
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-04-04 17:33:12 +0530

    destrcture

--- Assignment-2/src/components/UsersList.tsx
+++ Assignment-2/src/components/UsersList.tsx
@@ -5,29 +5,29 @@ const UsersList = () => {
 
   return (
     <div className="w-full h-auto flex flex-row gap-3 justify-center m-4">
-      {UsersData.map((data) => (
+      {UsersData.map(({id, name, email, age, location}) => (
         <div className=" flex flex-col items-start bg-black mt-5 text-white p-8 rounded-2xl">
           <div className="flex flex-row gap-8 my-4">
             <h1 className="text-xl">
               <span className="text-red-500 font-bold">Id: </span>
-              {data.id}
+              {id}
             </h1>
             <h2 className="text-xl">
               <span className="text-red-500 font-bold">Name: </span>
-              {data.name}
+              {name}
             </h2>
           </div>
           <p>
             <span className="text-blue-500 font-bold">Email: </span>
-            {data.email}
+            {email}
           </p>
           <p>
             <span className="text-blue-500 font-bold">Age: </span>
-            {data.age}
+            {age}
           </p>
           <p>
             <span className="text-blue-500 font-bold">Location: </span>
-            {data.location}
+            {location}
           </p>
         </div>
       ))}
