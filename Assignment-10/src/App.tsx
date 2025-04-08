a3929daa9689ae589f22fcfd0eb5cc176bbf9b67
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-04-08 10:36:43 +0530

    .

---
+++ Assignment-10/src/App.tsx
@@ -0,0 +1,13 @@
+// Todo List app
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen bg-gray-900 text-white flex flex-col items-center pt-44">
+        <h1 className="text-2xl font-bold mb-6">Todo App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
--- Assignment-9/src/components/AndAndButton.tsx
+++ Assignment-9/src/components/AndAndButton.tsx
@@ -1,9 +1,7 @@
 const AndAndButton = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
   return (
     <div>
-      {isLoggedIn && (
-        <button className="bg-red-500 m-2 p-2 rounded-lg">Logout</button>
-      )}
+      {isLoggedIn && <h1 className="text-2xl font-bold mb-6 mt-6">Hello User!</h1>}
     </div>
   );
 };
