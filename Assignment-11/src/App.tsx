2963cf9db1b0b5432e5c9c053bd06a5a8191be67
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-04-08 14:54:14 +0530

    .

---
+++ Assignment-11/src/App.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [count, setCount] = useState<number>(0);
+
+  useEffect(() => {
+    console.log("Count : ", count);
+
+    return () => {
+      console.log("Clean up!");
+    };
+  }, [count]);
+
+  return (
+    <>
+      <div className="w-full min-h-screen bg-gray-900 text-white flex flex-col items-center pt-44">
+        <h1 className="text-4xl font-bold mb-6 text-blue-500">useEffect</h1>
+
+        <h2 className="text-2xl font-bold mb-6 text-gray-500">
+          Counter: <span className="text-white">{count}</span>
+        </h2>
+        <div className="flex flex-row justify-center items-center">
+          <button
+            onClick={() => setCount(count + 1)}
+            className="m-4 p-2 px-8 rounded-lg border-2 border-gray-400 bg-gray-600 "
+          >
+            Increment
+          </button>
+          <button
+            onClick={() => setCount(count - 1)}
+            className="m-4 p-2 px-8 rounded-lg border-2 border-gray-400 bg-gray-600 "
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
