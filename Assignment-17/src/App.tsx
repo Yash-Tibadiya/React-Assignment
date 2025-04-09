c7524b49cdd3cd01d2e1ae8775a59cacae79847e
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-04-09 15:17:01 +0530

    .

--- Assignment-17/src/App.tsx
+++ Assignment-17/src/App.tsx
@@ -1,10 +1,40 @@
+// useCallback - it is used to memoize a function itself to avoid re-creating it unless dependencies change.
+
+import "./App.css";
+import { useState, useCallback } from "react";
+import { shuffle } from "./utils";
+import Search from "./Search";
+
+const allUsers = ["yash", "rohit", "meet", "aman", "ravi"];
 
 function App() {
+  const [users, setUsers] = useState(allUsers);
+
+  const handleSearch = useCallback(
+    (text: string) => {
+      console.log(users[0]);
+
+      const filteredUsers = allUsers.filter((user) => user.includes(text));
+      setUsers(filteredUsers);
+    },
+    // []
+    [users]
+  );
+
   return (
-    <>
-      <div>hi</div>
-    </>
+    <div className="card">
+      <div>
+        <button onClick={() => setUsers(shuffle(allUsers))}>Shuffle</button>
+        <Search onChange={handleSearch} />
+      </div>
+      <ul>
+        {users.map((user) => (
+          <li key={user}>{user}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
 export default App;
+
