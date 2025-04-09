55013b3074b92d91be677d6eaea8ce96f79c8e0b
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-04-09 16:23:11 +0530

    .

--- Assignment-18/src/App.tsx
+++ Assignment-18/src/App.tsx
@@ -1,6 +1,26 @@
 // Search
 
+import { useRef, useState } from "react";
+
 function App() {
+  const [items, setItems] = useState<string[]>([]);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleAddItem = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const value = inputRef.current?.value?.trim();
+    if (!value) return;
+
+    setItems((prev) => {
+      return [...prev, value];
+    });
+
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  };
+
   return (
     <>
       <div className="w-full min-h-screen bg-gray-900 text-white flex flex-col items-center pt-44">
@@ -10,30 +30,55 @@ function App() {
             Add & Search Items
           </h1>
 
+          {/* Add New Item */}
           <div className="flex flex-col justify-start">
             <div className="ml-32 items-center mb-2">
-              <label htmlFor="task" className="font-bold text-2xl">
-                New Item :{" "}
-              </label>
-              <input
-                type="text"
-                placeholder="Enter Todo or Task"
-                className="p-2 m-4 ml-10 border-2 rounded-lg w-1/2"
-              />
-              <button className="bg-gray-900 p-2 px-5 rounded-lg">Add</button>
+              <form onSubmit={handleAddItem}>
+                <label htmlFor="task" className="font-bold text-2xl">
+                  New Item :{" "}
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter New Item"
+                  className="p-2 m-4 ml-10 border-2 rounded-lg w-1/2"
+                  ref={inputRef}
+                />
+                <button
+                  type="submit"
+                  className="bg-gray-900 p-2 px-5 rounded-lg"
+                >
+                  Add
+                </button>
+              </form>
             </div>
 
+            {/* Search Items */}
             <div className="ml-32 items-center mb-5">
               <label htmlFor="task" className="font-bold text-2xl">
                 Search Item :{" "}
               </label>
               <input
-                type="text"
-                placeholder="Enter Todo or Task"
+                type="search"
+                placeholder="Enter item you want to search"
                 className="p-2 m-4 border-2 rounded-lg w-1/2"
               />
             </div>
           </div>
+
+          {/* show Items */}
+          <h1 className="flex text-2xl font-bold p-2 m-2 mt-5 justify-center items-center text-blue-400">
+            All Items
+          </h1>
+          <ul className="flex flex-col justify-center m-2 mx-6 px-6">
+            {items.map((item: string, index: number) => (
+              <li
+                className="bg-gray-800 rounded-xl text-lg font-medium m-2 p-4 flex flex-row justify-between"
+                key={index}
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </>
