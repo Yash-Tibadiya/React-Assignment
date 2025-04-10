11e4d514e628ac0231f39f58a019e4a6a7da3415
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-04-10 10:21:28 +0530

    .

--- Assignment-20/src/App.tsx
+++ Assignment-20/src/App.tsx
@@ -1,9 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages.tsx/Home";
+import About from "./pages.tsx/About";
+import Navbar from "./components/Navbar";
+// import NotFoundPage from "./pages.tsx/NotFoundPage";
+
 function App() {
   return (
     <>
-      <div className="w-full h-screen bg-gray-800 flex justify-center ">
-        <h1 className="text-4xl h-96 text-white pt-36">Home Page</h1>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/*" element={<NotFoundPage />} /> */}
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
     </>
   );
 }
---
+++ Assignment-20/src/pages.tsx/About.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div className="w-full h-screen bg-gray-800 flex justify-center ">
+      <h1 className="text-4xl h-96 text-white pt-36">About Page</h1>
+    </div>
+  );
+};
+
+export default About;
