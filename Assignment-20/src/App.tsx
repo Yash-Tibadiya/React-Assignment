944ab1591e02336e3bf23bae175c30c7a3ce578d
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-04-10 11:30:59 +0530

    .

--- Assignment-20/src/App.tsx
+++ Assignment-20/src/App.tsx
@@ -1,21 +1,28 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Navbar from "./components/Navbar";
 import Community from "./pages/Community";
 import X from "./pages/Community/X";
 import Youtube from "./pages/Community/Youtube";
 import Github from "./pages/Community/Github";
 import Linkedin from "./pages/Community/Linkedin";
+import NavbarLayout from "./pages/NavbarLayout";
+// import Navbar from "./components/Navbar";
 // import NotFoundPage from "./pages.tsx/NotFoundPage";
 
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route element={<NavbarLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} /> */}
+
         <Route path="/community" element={<Community />}>
           <Route path="x" element={<X />} />
           <Route path="youtube" element={<Youtube />} />
